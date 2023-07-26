dc04bf87200f58db8d32521ad9620ad8b5ce2b47
Arcod7 <antoine.esman@epitech.eu>
2023-07-26 13:18:55 +0200

    [ADD] chat app

---
+++ lib/screens/chat.dart
@@ -0,0 +1,56 @@
+import 'package:zeeyou/widgets/chat_messages.dart';
+import 'package:zeeyou/widgets/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+
+    // final token = await fcm.getToken();
+    // print(token); // could send this to backend
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CallotChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary),
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
