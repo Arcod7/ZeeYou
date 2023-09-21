c5dce0094527688f59022d1b526d15f0f7ec9842
Antoine <antoine@olvidadmins-macbook-pro.home>
2023-09-21 09:47:54 +0200

    [ADD] Badges (no backend)

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:badges/badges.dart' as badges;
 
-class MessageBubble extends StatelessWidget {
+class MessageBubble extends StatefulWidget {
   const MessageBubble.first({
     super.key,
     required this.userImage,
@@ -31,19 +33,25 @@ class MessageBubble extends StatelessWidget {
   final CollectionReference<Map<String, dynamic>> chatCollectionRef;
 
   @override
+  State<MessageBubble> createState() => _MessageBubbleState();
+}
+
+class _MessageBubbleState extends State<MessageBubble> {
+  bool _isReactionVisible = false;
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
     return Stack(
       children: [
-        if (userImage != null)
+        if (widget.userImage != null)
           Positioned(
             top: 15,
             // Align user image to the right, if the message is from me.
-            right: isMe ? 0 : null,
+            right: widget.isMe ? 0 : null,
             child: CircleAvatar(
               backgroundImage: NetworkImage(
-                userImage!,
+                widget.userImage!,
               ),
               backgroundColor: theme.colorScheme.primary.withAlpha(180),
               radius: 23,
@@ -51,7 +59,10 @@ class MessageBubble extends StatelessWidget {
           ),
         GestureDetector(
           onDoubleTap: () {
-            chatCollectionRef.doc(messageId).delete();
+            setState(() {
+              _isReactionVisible = !_isReactionVisible;
+            });
+            // chatCollectionRef.doc(messageId).delete();
           },
           child: Container(
             // Add some margin to the edges of the messages, to allow space for the
@@ -60,23 +71,24 @@ class MessageBubble extends StatelessWidget {
             child: Row(
               // The side of the chat screen the message should show at.
               mainAxisAlignment:
-                  isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+                  widget.isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
               children: [
                 Column(
-                  crossAxisAlignment:
-                      isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                  crossAxisAlignment: widget.isMe
+                      ? CrossAxisAlignment.end
+                      : CrossAxisAlignment.start,
                   children: [
                     // First messages in the sequence provide a visual buffer at
                     // the top.
-                    if (isFirstInSequence) const SizedBox(height: 18),
-                    if (username != null)
+                    if (widget.isFirstInSequence) const SizedBox(height: 18),
+                    if (widget.username != null)
                       Padding(
                         padding: const EdgeInsets.only(
                           left: 13,
                           right: 13,
                         ),
                         child: Text(
-                          username!,
+                          widget.username!,
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             color: Colors.black87,
@@ -85,50 +97,72 @@ class MessageBubble extends StatelessWidget {
                       ),
 
                     // The "speech" box surrounding the message.
-                    Container(
-                      decoration: BoxDecoration(
-                        color: isMe
-                            ? Colors.grey[300]
-                            : theme.colorScheme.secondary.withAlpha(200),
-                        // Only show the message bubble's "speaking edge" if first in
-                        // the chain.
-                        // Whether the "speaking edge" is on the left or right depends
-                        // on whether or not the message bubble is the current user.
-                        borderRadius: BorderRadius.only(
-                          topLeft: !isMe && isFirstInSequence
-                              ? Radius.zero
-                              : const Radius.circular(12),
-                          topRight: isMe && isFirstInSequence
-                              ? Radius.zero
-                              : const Radius.circular(12),
-                          bottomLeft: const Radius.circular(12),
-                          bottomRight: const Radius.circular(12),
+                    badges.Badge(
+                      badgeContent: Icon(
+                        MdiIcons.heart,
+                        size: 18,
+                        color: Colors.red,
+                      ),
+                      badgeStyle: const badges.BadgeStyle(
+                        borderSide: BorderSide(
+                          color: Colors.blueGrey,
+                          width: 1,
                         ),
+                        badgeColor: Colors.white,
                       ),
-                      // Set some reasonable constraints on the width of the
-                      // message bubble so it can adjust to the amount of text
-                      // it should show.
-                      constraints: const BoxConstraints(maxWidth: 200),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 14,
-                      ),
-                      // Margin around the bubble.
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 4,
-                        horizontal: 12,
-                      ),
-                      child: Text(
-                        message,
-                        style: TextStyle(
-                          // Add a little line spacing to make the text look nicer
-                          // when multilined.
-                          height: 1.3,
-                          color: isMe
-                              ? Colors.black87
-                              : theme.colorScheme.onSecondary,
+                      position: badges.BadgePosition.bottomStart(bottom: 0, start: 10),
+                      // backgroundColor: Colors.white,
+                      // offset: const Offset(20, -5),
+                      // alignment: Alignment.bottomLeft,
+                      // largeSize: 26,
+                      showBadge: _isReactionVisible,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: widget.isMe
+                              ? Colors.grey[300]
+                              : theme.colorScheme.secondary.withAlpha(200),
+                          // Only show the message bubble's "speaking edge" if first in
+                          // the chain.
+                          // Whether the "speaking edge" is on the left or right depends
+                          // on whether or not the message bubble is the current user.
+                          borderRadius: BorderRadius.only(
+                            topLeft: !widget.isMe && widget.isFirstInSequence
+                                ? Radius.zero
+                                : const Radius.circular(12),
+                            topRight: widget.isMe && widget.isFirstInSequence
+                                ? Radius.zero
+                                : const Radius.circular(12),
+                            bottomLeft: const Radius.circular(12),
+                            bottomRight: const Radius.circular(12),
+                          ),
                         ),
-                        softWrap: true,
+                        // Set some reasonable constraints on the width of the
+                        // message bubble so it can adjust to the amount of text
+                        // it should show.
+                        constraints: const BoxConstraints(maxWidth: 200),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 14,
+                        ),
+                        // Margin around the bubble.
+                        margin: EdgeInsets.only(
+                          top: 4,
+                          left: 12,
+                          right: 12,
+                          bottom: _isReactionVisible ? 20 : 4,
+                        ),
+                        child: Text(
+                          widget.message,
+                          style: TextStyle(
+                            // Add a little line spacing to make the text look nicer
+                            // when multilined.
+                            height: 1.3,
+                            color: widget.isMe
+                                ? Colors.black87
+                                : theme.colorScheme.onSecondary,
+                          ),
+                          softWrap: true,
+                        ),
                       ),
                     ),
                   ],
